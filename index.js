461feb58dee19eb1e34f6757d3f25f1a3f2e1f66
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2023-01-07 21:54:18 +0200

    added dotenv package

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`App listening at :${PORT}PORT`));
+app.listen(PORT, () => console.log(`App listening at :${PORT} PORT`));
