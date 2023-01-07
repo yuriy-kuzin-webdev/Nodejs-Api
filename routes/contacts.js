da5396ab8bcd23ecadd263da1010cdcb0692395e
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2023-01-07 22:30:15 +0200

    updated router

--- routes/contacts.js
+++ routes/contacts.js
@@ -8,10 +8,7 @@ const {
 } = require("../controllers/contacts");
 const router = express.Router();
 
-router.route("/").get(getContact);
-router.route("/:id").get(getContacts);
-router.route("/").post(createContact);
-router.route("/:id").put(updateContact);
-router.route("/:id").delete(deleteContact);
+router.route("/").get(getContacts).post(createContact);
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
 module.exports = router;
