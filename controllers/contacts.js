7a058bcb2fd8a9d03560a01e597a1f1205f7e802
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2023-01-08 00:27:11 +0200

    added create/ get all

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,18 +1,27 @@
 const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contact");
 
 const getContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "get contact" });
 });
 const getContacts = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "get contacts" });
+  const contacts = await Contact.find();
+  res.status(200).json(contacts);
 });
 const createContact = asyncHandler(async (req, res) => {
+  console.log("The request body is :", req.body);
   const { name, email, phone } = req.body;
   if (!name || !email || !phone) {
     res.status(400);
     throw new Error("All fields are mandatory !");
   }
-  res.status(200).json({ message: "Create contact" });
+  const contact = await Contact.create({
+    name,
+    email,
+    phone
+  });
+
+  res.status(201).json(contact);
 });
 const updateContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Update contact" });
