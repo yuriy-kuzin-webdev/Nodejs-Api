81a57cb245b9a8077c18c9b45892e674041e4e96
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2023-01-08 13:02:26 +0200

    updated users router

--- routes/users.js
+++ routes/users.js
@@ -4,3 +4,11 @@ const router = express.Router();
 router.post("/register", (req, res) => {
   res.json({ message: "register user" });
 });
+
+router.post("/login", (req, res) => {
+  res.json({ message: "login user" });
+});
+
+router.post("/current", (req, res) => {
+  res.json({ message: "current user" });
+});
