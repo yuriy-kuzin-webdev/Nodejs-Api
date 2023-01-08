d8270f17d899bd541b78a76e8fb5d07a70fef0b2
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2023-01-08 13:01:24 +0200

    added user router

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ connectDb()
 const app = express();
 
 const contactsRouter = require("./routes/contacts");
+const usersRouter = require("./routes/users");
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(errorHandler);
 app.use("/api/contacts", contactsRouter);
+app.use("/api/users", usersRouter);
 
 app.listen(PORT, () => console.log(`App listening at :${PORT} PORT`));
